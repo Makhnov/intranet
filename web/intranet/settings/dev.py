ce1d38bf09eae05728183202983694cbf1095105
Makhnov <service.informatique@cagiregaronnesalat.fr>
2024-02-24 19:02:56 +0100

    gitignore (static)

--- web/intranet/settings/dev.py
+++ web/intranet/settings/dev.py
@@ -6,10 +6,10 @@ DEBUG = True
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-jc$23*h%wtjs&4=+2x$ovklg#wlemz07_7p=5bar89u+e*dr+d"
 
-
 # SECURITY WARNING: define the correct hosts in production!
 ALLOWED_HOSTS = ["*"]
 
+# EMAIL
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 # Général
