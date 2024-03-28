9e07df3fb31485f752fdf524e8ef884fca310264
Clémence <service.informatique@cagiregaronnesalat.fr>
2024-03-28 12:17:42 +0100

    s

--- web/intranet/settings/base.py
+++ web/intranet/settings/base.py
@@ -348,12 +348,12 @@ DEFAULT_AUTO_FIELD = (
 
 # Mailing
 EMAIL_BACKEND = os.getenv("WAGTAIL_EMAIL_BACKEND", "django.core.mail.backends.console.EmailBackend")
+EMAIL_USE_TLS = os.getenv("WAGTAIL_EMAIL_USE_TLS", False)
+EMAIL_USE_SSL = os.getenv("WAGTAIL_EMAIL_USE_SSL", True)
 EMAIL_HOST = os.getenv("WAGTAIL_EMAIL_HOST", "smtp.gmail.com")
 EMAIL_PORT = os.getenv("WAGTAIL_EMAIL_PORT", 587)
 EMAIL_HOST_USER = os.getenv("WAGTAIL_EMAIL_HOST_USER", "wagtail_example.com")
 EMAIL_HOST_PASSWORD = os.getenv("WAGTAIL_EMAIL_HOST_PASSWORD", "password")
-EMAIL_USE_TLS = os.getenv("WAGTAIL_EMAIL_USE_TLS", False)
-EMAIL_USE_SSL = os.getenv("WAGTAIL_EMAIL_USE_SSL", True)
 DEFAULT_FROM_EMAIL = os.getenv("WAGTAIL_DEFAULT_FROM_EMAIL", "wagtail_example.com")
 
 # PDF2Images
