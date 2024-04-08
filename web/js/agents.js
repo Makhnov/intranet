782d805c55ddd6ad9fc7d6de65e34087c8e4628b
Makhnov <service.informatique@cagiregaronnesalat.fr>
2024-04-08 08:15:23 +0200

    reset migrations

--- web/js/agents.js
+++ web/js/agents.js
@@ -144,11 +144,11 @@ function cgsItem() {
         const optionsBlock = block.querySelector("ul.choice-list");
         const contentBlock = block.querySelector("div.cgs-choice-content");
 
-        console.log("not reduced", block);
+        // console.log("not reduced", block);
         const options = optionsBlock.querySelectorAll("ul.choice-list li");
-        console.log(options);
+        // console.log(options);
         const content = contentBlock.querySelectorAll("div.cgs-choice-content > div");
-        console.log(content);
+        // console.log(content);
 
         // Ajout de l'écouteur d'événement sur les liens
         options.forEach(option => {
@@ -168,15 +168,14 @@ function cgsItem() {
                 }
             });
         });
-        
-    
+
         // Créer un nouvel observateur de redimensionnement
         const resizeObserver = new ResizeObserver(entries => {
             for (let entry of entries) {
                 const {width} = entry.contentRect;
                 // on récupère le padding (variable root : --padding-choice)
                 const padding = getComputedStyle(contentBlock).getPropertyValue('--padding-choice');
-                console.log(`La nouvelle largeur des options est : ${width}`);
+                // console.log(`La nouvelle largeur des options est : ${width}`);
                 contentBlock.style.width = `calc(${width - 2}px - ${padding} * 2)`;
             }
         });
@@ -187,7 +186,32 @@ function cgsItem() {
     
     function manageChoiceBlockReduced(block) {        
         console.log("reduced", block);
-        const links = block.querySelectorAll("ul.choice-list li");
-        console.log(links);
+        const optionsBlockReduced = block.querySelector("ul.choice-list");
+        const contentBlockReduced = block.querySelector("div.cgs-choice-content");
+
+        console.log("Reduced", block);
+        const reducedOptions = optionsBlockReduced.querySelectorAll("ul.choice-list li");
+        console.log(reducedOptions);
+        const reducedContent = contentBlockReduced.querySelectorAll("div.cgs-choice-content > div");
+        console.log(reducedContent);
+
+        // Ajout de l'écouteur d'événement sur les liens
+        reducedOptions.forEach(option => {
+            option.addEventListener('click', function() {
+                if (!this.classList.contains('cgs-selected')) {
+                    reducedOptions.forEach(l => l.classList.remove('cgs-selected'));
+                    this.classList.add('cgs-selected');
+                    const targetId = this.getAttribute('data-target');
+
+                    reducedContent.forEach(block => {
+                        if (block.id === targetId) {
+                            block.classList.remove('cgs-hidden');
+                        } else {
+                            block.classList.add('cgs-hidden');
+                        }
+                    });
+                }
+            });
+        });    
     }
 }
